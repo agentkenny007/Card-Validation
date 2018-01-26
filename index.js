d0ef24195d5b54b557520d798cccd3258cb8b4fe
Ikenna Ugwuh <ikennaugwuh@gmail.com>
2018-01-26 03:53:31 -0500

    Added a few validations to . Now works with space separated credit card numbers.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let luhn = cardNum => cardNum
-    .split('').reverse()
-    .map((digit, index) => index % 2 == 1 ?
-        (Number(digit) * 2).toString().length > 1 ?
-            Number((Number(digit) * 2).toString()[0]) + Number((Number(digit) * 2).toString()[1])
-            : Number(digit) * 2
-        : Number(digit))
+let luhn = cardNum => cardNum && typeof cardNum === 'string' && Number(cardNum.replace(/\s/g,'')) && cardNum
+    .replace(/\s/g, '').split('').reverse()
+    .map((digit, index) => index % 2 === 1 ?
+      (Number(digit) * 2).toString().length > 1 ?
+        Number((Number(digit) * 2).toString()[0]) + Number((Number(digit) * 2).toString()[1])
+        : Number(digit) * 2
+      : Number(digit))
     .reduce((a, b) => a + b) % 10 === 0;
 
 (function validateCard(){
